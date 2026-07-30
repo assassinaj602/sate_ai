63d3fd5610065f74a5cf60d87fe21a6123918f73
Muhammad Assad Ullah <150547310+assassinaj602@users.noreply.github.com>
2026-07-30 13:51:52 +0500

    fix: suppress avoid_print lint in CLI binary (legitimate print usage)

--- bin/sate_ai.dart
+++ bin/sate_ai.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: avoid_print, prefer_const_constructors
 import 'dart:io';
 import 'package:args/args.dart';
 import 'package:sate_ai/sate_ai_cli.dart';
